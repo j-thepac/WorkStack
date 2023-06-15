de01a0bba5ac928387b4795debef8b64e5a9ece0
Deepak Jayaprakash <deepakjayaprakash@Deepaks-MacBook-Air.local>
2023-06-15 14:18:59 +0530

    working code

---
+++ Server.py
@@ -0,0 +1,33 @@
+from flask import Flask,request,json
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+
+import logging 
+logging.basicConfig(level=logging.INFO )
+
+app=Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///workdescdb.sqlite3"
+api=Api(app)
+db= SQLAlchemy(app)
+
+
+class WorkDB(db.Model):
+    sl=db.Column(db.Integer,primary_key=True)
+    desc=db.Column(db.String(1000))
+
+class WorkStack(Resource):
+    def post(self):
+        lastKey=len(WorkDB.query.all())
+        logging.info(f"{request.json}")
+        w:WorkDB=WorkDB(sl=lastKey,desc=request.json["desc"])
+        db.session.add(w)
+        db.session.commit()
+        return {"dataAdded":f"{request.json['desc']}" , "status":"Success"}
+        # workdb:WorkDB=WorkDB(sl=lastKey+1,desc=request.json())
+
+
+api.add_resource(WorkStack,"/")
+if(__name__=="__main__"):
+    app.app_context().push()
+    db.create_all()
+    app.run(host="0.0.0.0",port="4321")
